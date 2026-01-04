6e9bcc2523ae14876a0871a54956bb1c0af1e7eb
tnur2024-pixel <tnur2024@gmail.com>
2026-01-04 09:52:52 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// 🔹 MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// 🔹 Simple test route
+app.get("/", (req, res) => {
+  res.send("TG Monetag Bot Backend Running");
+});
+
+// 🔹 Port (Render uses this)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
